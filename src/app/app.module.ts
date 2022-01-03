bcda4b62a4354491d48599bb19b2f4ccf6826699
Camila Fonseca <camilarqf@gmail.com>
2022-01-03 12:17:56 -0300

    adiciona componente NAV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavComponent } from './components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
