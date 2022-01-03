1cdc5caf943124090939eabc401776499c37f106
Camila Fonseca <camilarqf@gmail.com>
2022-01-03 16:54:24 -0300

    edita conteudo de tabela em TECNICO-LIST

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Tecnico } from './../../../models/tecnico';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [{
+    id: 1,
+    nome: 'Teste',
+    cpf: "123.456.789-10",
+    email: "teste@email.com",
+    senha: "1234",
+    perfis: ['0'],
+    dataCriacao: "03/01/2022"
+  }]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
