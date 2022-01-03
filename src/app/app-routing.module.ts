d8c087794888f4c1e62bae861b5c66fd35a0e7ab
Camila Fonseca <camilarqf@gmail.com>
2022-01-03 14:26:45 -0300

    adiciona rota HOME

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from "./components/home/home.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
@@ -6,6 +7,7 @@ const routes: Routes = [
   {
     path: "",
     component: NavComponent,
+    children: [{ path: "home", component: HomeComponent }],
   },
 ];
 
