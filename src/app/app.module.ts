924908b8e511958e6083d571147df2d6fb8c4b0a
Camila Fonseca <camilarqf@gmail.com>
2022-01-04 20:47:47 -0300

    serviço de mensagens implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login/login.component';
-import { NavComponent } from './components/nav/nav.component';
-import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login/login.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
 
 @NgModule({
   declarations: [
@@ -32,7 +32,7 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
     HomeComponent,
     HeaderComponent,
     TecnicoListComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -53,9 +53,14 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
     MatListModule,
     MatCardModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/login/login/login.component.ts
+++ src/app/components/login/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Credenciais } from "./../../../models/credenciais";
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: "app-login",
@@ -14,9 +15,14 @@ export class LoginComponent implements OnInit {
   }
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
-  constructor() {}
+  constructor(private toastr:ToastrService) {}
 
   ngOnInit(): void {}
+
+  logar(){
+    this.toastr.error("Usuário e/ou senha inválidos!", "Login")
+    this.login.senha = "";
+  }
 
   validaCampos():boolean {
     if(this.email.valid && this.senha.valid){
