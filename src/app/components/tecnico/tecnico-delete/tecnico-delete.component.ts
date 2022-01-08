a84f2aabc9876a7e31c64a8c6e3af9c568db4758
Camila Fonseca <camilarqf@gmail.com>
2022-01-08 12:41:13 -0300

    finaliza componente TECNICO-DELETE

--- src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+
+import { Tecnico } from "./../../../../models/tecnico";
+import { TecnicoService } from "./../../../services/tecnico.service";
 
 @Component({
   selector: 'app-tecnico-delete',
@@ -6,10 +13,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-delete.component.css']
 })
 export class TecnicoDeleteComponent implements OnInit {
+  tecnico: Tecnico = {
+    id: "",
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    perfis: [],
+    dataCriacao: "",
+  };
 
-  constructor() { }
+  constructor(
+    private service: TecnicoService,
+    private toastr: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.tecnico.id = this.route.snapshot.paramMap.get("id");
+    this.findById();
+  }
+  checked = false;
+
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe((response) => {
+      response.perfis = [];
+      this.tecnico = response;
+    });
+  }
+
+  delete(): void {
+    this.service.delete(this.tecnico.id).subscribe(
+      (response) => {
+        this.toastr.success("Técnico deletado com sucesso", "Delete");
+        this.router.navigate(["tecnicos"]);
+      },
+      (ex) => {
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element) => {
+            this.toastr.error(element.message);
+          });
+        } else {
+          this.toastr.error(ex.error.message);
+        }
+      }
+    );
   }
 
 }
