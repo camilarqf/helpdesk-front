74f89160ca37e5e1e09e08bfd14896b0c42a7647
Camila Fonseca <camilarqf@gmail.com>
2022-01-09 15:29:29 -0300

    adiciona rota componente CLIENTES-LIST

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
+import { TecnicoDeleteComponent } from "./components/tecnico/tecnico-delete/tecnico-delete.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AuthGuard } from './auth/auth.guard';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login/login.component';
-import { NavComponent } from './components/nav/nav.component';
-import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
-import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
-import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
+import { AuthGuard } from "./auth/auth.guard";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login/login.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { TecnicoCreateComponent } from "./components/tecnico/tecnico-create/tecnico-create.component";
+import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
+import { TecnicoUpdateComponent } from "./components/tecnico/tecnico-update/tecnico-update.component";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
@@ -25,7 +26,11 @@ const routes: Routes = [
       },
       { path: "tecnicos/create", component: TecnicoCreateComponent },
       { path: "tecnicos/update/:id", component: TecnicoUpdateComponent },
-      { path: "tecnicos/delete/:id", component: TecnicoDeleteComponent }
+      { path: "tecnicos/delete/:id", component: TecnicoDeleteComponent },
+      {
+        path: "clientes",
+        component: ClienteListComponent,
+      },
     ],
   },
 ];
--- src/app/components/cliente/cliente-list/cliente-list.component.ts
+++ src/app/components/cliente/cliente-list/cliente-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { Tecnico } from '../../../../models/tecnico';
+import { TecnicoService } from './../../../services/tecnico.service';
 
 @Component({
   selector: 'app-cliente-list',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-list.component.css']
 })
 export class ClienteListComponent implements OnInit {
+  ELEMENT_DATA: Tecnico[] = [];
 
-  constructor() { }
+  displayedColumns: string[] = ["ID", "nome", "CPF", "Email", "acoes"];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  constructor(private service: TecnicoService) {}
 
   ngOnInit(): void {
+    this.findAll();
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  findAll() {
+    this.service.findAll().subscribe((response) => {
+      this.ELEMENT_DATA = response;
+      this.dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
