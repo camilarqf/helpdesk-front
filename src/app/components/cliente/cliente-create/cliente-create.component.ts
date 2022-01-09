9f08ce1af5b161a257abb7bf42f27687401ece8b
Camila Fonseca <camilarqf@gmail.com>
2022-01-09 16:26:23 -0300

    finaliza componente CLIENTE-CREATE

--- src/app/components/cliente/cliente-create/cliente-create.component.ts
+++ src/app/components/cliente/cliente-create/cliente-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { Cliente } from 'src/models/cliente';
 
 @Component({
   selector: 'app-cliente-create',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteCreateComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente = {
+    id: "",
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    perfis: [],
+    dataCriacao: "",
+  };
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  ngOnInit(): void {
+  constructor(
+    private service: ClienteService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+  checked = false;
+
+  create(): void {
+    this.service.create(this.cliente).subscribe(
+      (response) => {
+        this.toastr.success("Técnico cadastrado com sucesso", "Cadastro");
+        this.router.navigate(["clientes"]);
+      },
+      (ex) => {
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element) => {
+            this.toastr.error(element.message);
+          });
+        } else {
+          this.toastr.error(ex.error.message);
+        }
+      }
+    );
   }
 
+  addPerfil(perfil: any): void {
+    if (this.cliente.perfis.includes(perfil)) {
+      this.cliente.perfis.splice(this.cliente.perfis.indexOf(perfil), 1);
+    } else {
+      this.cliente.perfis.push(perfil);
+    }
+  }
+
+  validaCampos() {
+    return (
+      this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+    );
+  }
 }
