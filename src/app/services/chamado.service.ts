07d3b55c828c73cb92162ad0a1e92e144211da5c
Camila Fonseca <camilarqf@gmail.com>
2022-01-09 18:48:43 -0300

    finaliza componente CHAMADO-LIST

---
+++ src/app/services/chamado.service.ts
@@ -0,0 +1,16 @@
+import { Chamado } from "./../../models/chamado";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { API_CONFIG } from "../config/api.config";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ChamadoService {
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<Chamado[]> {
+    return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
+  }
+}
