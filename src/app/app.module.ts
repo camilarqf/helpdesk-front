d136d3da661235bed02667cfd4b2cc93ec693729
Camila Fonseca <camilarqf@gmail.com>
2022-01-10 21:16:21 -0300

    adiciona componente CHAMADO-READ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 
 @NgModule({
   declarations: [
@@ -56,6 +57,7 @@ import { ChamadoUpdateComponent } from './components/chamado/chamado-update/cham
     ChamadoListComponent,
     ChamadoCreateComponent,
     ChamadoUpdateComponent,
+    ChamadoReadComponent,
   ],
   imports: [
     BrowserModule,
